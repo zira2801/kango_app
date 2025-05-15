783bf2709af850dd2ee224f3216ce4e2e88fb4fc
zira2801 <131445103+zira2801@users.noreply.github.com>
2025-05-14 19:53:03 -0700

    Update index.dart

--- lib/data/env/index.dart
+++ lib/data/env/index.dart
@@ -8,8 +8,7 @@ const String baseUrl = 'https://kangov2.websitehoconline.com/api/';
 // const String baseUrl = 'https://app.kango-post.com/api/';
 const String baseUrl2 = 'https://tracking.ksnpost.com/api/';
 const String httpImage = 'https://kangov2.websitehoconline.com/';
-const String googleMapApiKey = 'AIzaSyAU4Gzc5BpgYWvH7P0hqskwIuRwmr2qX20';
-const String googleVisionApi = 'AIzaSyBvd5KWRWwsico-6a1J-ePPzdnU520sJ8w';
+
 
 // const String baseUrl = 'http://192.168.3.17:8000/api/';
 // const String httpImage = 'http://192.168.3.17:8000/';
